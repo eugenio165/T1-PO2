db55ddae9cd211048aa0eb5d2e9b7c6f4f509a64
Antonio Eugenio <eugenio165.as@gmail.com>
2019-03-30 01:45:18 -0300

    165000317: Criando Componente de apresentação

---
+++ src/app/pages/apresentacao/apresentacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apresentacao',
+  templateUrl: './apresentacao.component.html',
+  styleUrls: ['./apresentacao.component.scss']
+})
+export class ApresentacaoComponent {
+
+  constructor() { }
+
+}
